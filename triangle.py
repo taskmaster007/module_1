3ff0f59dba0710aa745f55a6649d93e05613f93b
taskmaster007 <harshalborhade002@gmail.com>
2018-09-30 00:56:35 +0530

    added assignment unnamed

---
+++ triangle.py
@@ -0,0 +1,8 @@
+height = int(input("Enter Height of Triangle:"))
+base = int(input("Enter length of base of Triangle:"))
+print("Area of Triangle is:",0.5 * base * height)
+
+# OUTPUT
+# Enter Height of Triangle:65
+# Enter length of base of Triangle:21
+# Area of Triangle is: 682.5
